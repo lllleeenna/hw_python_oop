d6907a875e9a784cf2b76381b8a9c8a87d814d3f
Elena Smurova <lllleeenna@yandex.ru>
2022-08-25 02:37:24 +0700

    Комит с отступом

--- homework.py
+++ homework.py
@@ -118,7 +118,7 @@ class Swimming(Training):
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
         return (self.length_pool * self.count_pool / self.M_IN_KM
-               / self.duration)
+                / self.duration)
 
     def get_spent_calories(self) -> float:
         coeff_calorie_1 = 1.1
